8dd1f4bd28816f9a35d6c792c885687b76ef95e8
Patrick Albano <119360438+Floopy891@users.noreply.github.com>
2023-03-19 12:51:38 -0400

    Begin SearchResults.Js Class Component

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SearchResults.css';
+
+class SearchResults extends React.Component {
+
+    render() {
+        <div className="SearchResults">
+            <h2>Results</h2>
+            {/* <Tracklist /> */}
+        </div>
+    }
+}
