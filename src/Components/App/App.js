2c5918c2cd7ac8dc2ce809283853e31a1eb6ae04
Patrick Albano <119360438+Floopy891@users.noreply.github.com>
2023-03-19 12:34:30 -0400

    Create App class in app.js, import react classes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,24 @@
+import React from 'react';
+import './App.css';
+import SearchBar from './Components/SearchBar/SearchBar.js';
+import Playlist from './Components/Playlist/Playlist.js';
 
-
-function App() {
+class App extends React.Component {
+  
+  render () {
   return (
-<div>
-  <h1>Ja<span className="highlight">mmm</span>ing</h1>
-  <div className="App">
-    <input placeholder='Enter a Song Title'>
-      <a>SEARCH</a>
-    </input>
-    <div className="App-playlist">
-      <h2>Results</h2>
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <SearchBar />
+        <div className="App-playlist">
+          <SearchResults />
+          <Playlist />
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
+  }
 }
 
 export default App;
