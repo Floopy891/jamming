2a3f193132a4601988d3703574d778e22d3f669b
Patrick Albano <119360438+Floopy891@users.noreply.github.com>
2023-03-19 12:47:32 -0400

    Add css for SearchBar, fix import to App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar.js';
 
 class App extends React.Component {
   
@@ -8,7 +9,7 @@ class App extends React.Component {
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-        {/* <SearchBar /> */}
+        <SearchBar />
         <div className="App-playlist">
           {/* <SearchResults /> */}
           {/* <Playlist /> */}
