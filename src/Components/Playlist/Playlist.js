004ba88dd6259f1c587916623ebae5910b4ea5d8
Patrick Albano <119360438+Floopy891@users.noreply.github.com>
2023-03-22 18:21:11 -0400

    add playlist name change method

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -4,11 +4,20 @@ import TrackList from '../TrackList/TrackList';
 
 class Playlist extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(event) {
+        this.props.onNameChange(event.target.value);
+    }
+
     render() {
         return (
             <div className="Playlist">
-                <input value="New Playlist" />
-                <TrackList tracks={this.props.playlistTracks}  onRemove={this.props.onRemove} isRemoval={true} />
+                <input value="New Playlist" onChange={this.handleNameChange}/>
+                <TrackList tracks={this.props.playlistTracks}  onRemove={this.props.onRemove} isRemoval={true} onChange={this.handleNameChange}/>
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
