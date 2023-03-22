990510601e61b3ba950a392065ecb6e0c8017356
Patrick Albano <119360438+Floopy891@users.noreply.github.com>
2023-03-22 16:50:06 -0400

    addTrack method made set and passed

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,34 +5,45 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
-  
+
   constructor(props) {
     super(props);
     this.state = {
-      searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, 
-      {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, 
-      {name: 'name3', artist: 'artist3', album: 'album3', id: 3},
-      {name: 'name4', artist: 'artist4', album: 'album4', id: 4}],
+      searchResults: [{ name: 'name1', artist: 'artist1', album: 'album1', id: 1 },
+      { name: 'name2', artist: 'artist2', album: 'album2', id: 2 },
+      { name: 'name3', artist: 'artist3', album: 'album3', id: 3 },
+      { name: 'name4', artist: 'artist4', album: 'album4', id: 4 }],
       playlistName: 'My Jams',
-      playlistTracks: [{name: 'name5', artist: 'artist5', album: 'album5', id: 5},
-      {name: 'name6', artist: 'artist6', album: 'album6', id: 6},
-      {name: 'name7', artist: 'artist7', album: 'album7', id: 7}]
-  }
+      playlistTracks: [{ name: 'name5', artist: 'artist5', album: 'album5', id: 5 },
+      { name: 'name6', artist: 'artist6', album: 'album6', id: 6 },
+      { name: 'name7', artist: 'artist7', album: 'album7', id: 7 }]
+    };
+
+    this.addTrack = this.addTrack.bind(this);
+
   }
 
-  render () {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults searchResults={this.state.searchResults}/>
-          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+    this.state.track
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
   }
 }
 
